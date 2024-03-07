da02274ce4d4055b0e04af9dd95c6b88045286eb
Daazd <zoomie067@yahoo.com>
2024-03-07 11:27:59 -0700

    Activate Changes to Lightbulb Selections

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Write your code here
+const lightbulb1= document.getElementById('lightbulb1');
+const lightbulb2= document.getElementById('lightbulb2');
+const lightbulb3= document.getElementById('lightbulb3');
+const subtitle= document.querySelector('.subtitle');
+
+let count = 0;
+
+function updateSubtitle() {
+    subtitle.textContent = `You have clicked ${count} times`;
+}
+
+function handleClick(lightbulb) {
+    lightbulb.classList.toggle('active');
+    count++;
+    updateSubtitle();
+}
+
+lightbulb1.addEventListener('click', () => handleClick(lightbulb1));
+lightbulb2.addEventListener('click', () => handleClick(lightbulb2));
+lightbulb3.addEventListener('click', () => handleClick(lightbulb3));
